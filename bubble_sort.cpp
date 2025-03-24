aec66a888aad2bf9a1ead203a5cbe5a38ec0ec20
[Siddhant Raju] <[siddhantraju2003@gmail.com]>
2025-03-24 12:53:12 +0530

    changes in bubble_sort

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -9,7 +9,9 @@ public:
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (nums[j] > nums[j + 1]) {  
-                    swap(nums[j], nums[j + 1]);  
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
                 }
             }
         }
